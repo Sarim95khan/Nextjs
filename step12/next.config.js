177798c57edfc91dd6f41100ac92aaeaa9a89d61
Sarim95khan <sarim95khan@gmail.com>
2023-04-22 20:50:11 +0500

    removed config

--- step12/next.config.js
+++ step12/next.config.js
@@ -4,12 +4,7 @@ const nextConfig = {
     appDir: true,
   },
   env: {
-    // PGHOST: "ep-black-water-575258.us-east-2.aws.neon.tech",
-    // PGDATABASE: "table",
-    // PGUSER: "sarim95khan",
-    // PGPASSWORD: "gY94DfXoqnZb",
-    NEON_DATABASE_URL:
-      "postgres://sarim95khan:gY94DfXoqnZb@ep-black-water-575258.us-east-2.aws.neon.tech/table",
+    // DATABASE
   },
 };
 
